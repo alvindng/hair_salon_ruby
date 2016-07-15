fec19be0d0e614f0caf3eedec708263b33c41838
Alvin Ng <alvindng@gmail.com>
2016-07-15 13:16:28 -0700

    add integration spec and routing for creating a new stylist

--- app.rb
+++ app.rb
@@ -7,3 +7,25 @@ require('pg')
 require('pry')
 
 DB = PG.connect({:dbname => 'hair_salon_test'})
+
+get('/') do
+  erb(:index)
+end
+
+get('/stylists/new') do
+  erb(:stylists_form)
+end
+
+post('/stylists') do
+  name = params.fetch('name')
+  phone = params.fetch('phone')
+  stylist = Stylist.new(:name => name, :phone => phone, :id => nil)
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
