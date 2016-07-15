34b59333b56283a9039b49a182a7808ec913539a
Alvin Ng <alvindng@gmail.com>
2016-07-15 09:55:24 -0700

    add in the fetch function to get the id attribute for stylist

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,9 +1,10 @@
 class Stylist
-  attr_reader(:name, :phone)
+  attr_reader(:name, :phone, :id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @phone = attributes.fetch(:phone)
+    @id = attributes.fetch(:id)
   end
 
   define_singleton_method(:all) do
@@ -12,18 +13,21 @@ class Stylist
     returned_stylists.each() do |stylist|
       name = stylist.fetch('name')
       phone = stylist.fetch('phone')
-      stylists.push(Stylist.new({:name=> name, :phone => phone}))
+      id = stylist.fetch('id').to_i()
+      stylists.push(Stylist.new({:name=> name, :phone => phone, :id => id}))
     end
     stylists
   end
 
   define_method(:==) do |another_stylist|
-    self.name().==(another_stylist.name())
+    self.name().==(another_stylist.name()).&(self.id().==(another_stylist.id()))
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO stylists (name, phone) VALUES ('#{@name}', '#{@phone}');")
+    result = DB.exec("INSERT INTO stylists (name, phone) VALUES ('#{@name}', '#{@phone}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
   end
+
 
 
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -9,15 +9,15 @@ describe('Stylist') do
 
   describe('#==') do
     it('is the same stylist if they have the same name') do
-      stylist1 = Stylist.new(:name => "John Smith", :phone =>'312-867-5309')
-      stylist2 = Stylist.new(:name => "John Smith", :phone =>'312-867-5309')
+      stylist1 = Stylist.new(:name => "John Smith", :phone =>'312-867-5309', :id => nil)
+      stylist2 = Stylist.new(:name => "John Smith", :phone =>'312-867-5309', :id => nil)
       expect(stylist1).to(eq(stylist2))
     end
   end
 
   describe('#save') do
     it('saves the stylist to an array of stylists') do
-      test_stylist = Stylist.new(:name => "John Smith", :phone =>'312-867-5309')
+      test_stylist = Stylist.new(:name => "John Smith", :phone =>'312-867-5309', :id => nil)
       test_stylist.save()
       expect(Stylist.all()).to(eq([test_stylist]))
     end
