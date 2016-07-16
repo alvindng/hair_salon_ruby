01925b8808445f2f2ba4df78ea8ab48239f85826
Alvin Ng <alvindng@gmail.com>
2016-07-15 17:00:06 -0700

    update delete and edit ability for clients

--- lib/client.rb
+++ lib/client.rb
@@ -1,10 +1,11 @@
 class Client
-attr_reader(:name, :phone, :stylist_id)
+attr_reader(:name, :phone, :stylist_id, :id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @phone = attributes.fetch(:phone)
-    @stylist_id = attributes.fetch(:stylist_id).to_i()
+    @id = attributes[:id]
+    @stylist_id = attributes[:stylist_id].to_i()
   end
 
   define_method(:==) do |another_client|
@@ -18,13 +19,35 @@ attr_reader(:name, :phone, :stylist_id)
       name = client.fetch('name')
       phone = client.fetch('phone')
       stylist_id = client.fetch('stylist_id').to_i()
+      id = client.fetch('id').to_i()
       clients.push(Client.new({:name => name, :phone => phone, :stylist_id => stylist_id}))
     end
     clients
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO clients (name, phone, stylist_id) VALUES ('#{@name}', '#{@phone}', #{@stylist_id});")
+    DB.exec("INSERT INTO clients (name, phone, stylist_id) VALUES ('#{@name}', '#{@phone}', #{@stylist_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
   end
 
+  define_singleton_method(:find) do |id|
+    found_client = nil
+    Client.all().each() do |client|
+      if client.id().==(id)
+        found_client = client
+      end
+    end
+    found_client
+  end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM clients WHERE id = #{self.id()}")
+  end
+
+  define_method(:update) do |attributes|
+    @name = attributes.fetch(:name)
+    @phone = attributes.fetch(:phone)
+    @id = self.id()
+    DB.exec("UPDATE stylists SET (name,phone) = ('#{@name}','#{@phone}') WHERE id = #{@id};")
+  end
 end
